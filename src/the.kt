3ce345afcf82894cb2b0a4fbc2223f6a7dd3e12b
MIV228 <mivgithub@gmail.com>
2021-10-01 10:03:22 +0700

    huduifduygdytugjhddusffkuiuvkjduijkisscnkjfiuj.

--- src/the.kt
+++ src/the.kt
@@ -12,6 +12,7 @@ class the() {
         assertEquals(result, b4(d), "Access denied")
     }
 }
+
 fun main() {
     println("Решать дальше?(0/1)")
     var b = readLine()!!.toString()
@@ -25,6 +26,7 @@ fun main() {
                 "a2" -> a2()
                 "a18" -> a18()
                 "a65" -> a65()
+                "a66" -> a66()
                 else -> {
                     println("При вычислениях произошла ошибка.")
                     println("ВЫПОЛНЯТСЯ ПЕРЕЗАПУСК")
@@ -47,7 +49,8 @@ fun main() {
 
 
 }
-fun b4(d : Double): Double {
+
+fun b4(d: Double): Double {
     var pi = PI
     var L = pi * d
     return L
@@ -101,6 +104,20 @@ fun a65() {
     println(a)
     println(a2)
     main()
+}
+
+fun a66() {
+    var n = 10
+    var list = List(n) {
+        (1..9).random()
+    }
+    var a = list.find { it % 2 == 0 }
+    var l2 = list.map {
+        if ((it % 2) == 0) {
+        it + a!!
+        }
+    }
+    println(l2)
 }
 
 //fun a4(a : Int, b : Int) {
