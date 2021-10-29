c822e03cb53c366333e94ade2217aa613b7dd706
MIV228 <mivgithub@gmail.com>
2021-10-29 22:38:38 +0700

    updated pole file

--- src/pole.kt
+++ src/pole.kt
@@ -1,16 +1,17 @@
-@file:Suppress("UnusedEquals")
+@file:Suppress("UnusedEquals", "ASSIGNED_BUT_NEVER_ACCESSED_VARIABLE")
 
 import kotlin.system.exitProcess
 
 fun game() {
-    var n = 0
+    val n = 0
     var errCounter = 0
+    var points = 0
     while (n == 0) {
         if (errCounter >= 3) {
-
+            stopGame()
         }
     }
-    var strList = listOf<String>(
+    var strList = listOf(
         "арбуз",
         "сказка",
         "роль",
@@ -18,20 +19,23 @@ fun game() {
         "вирус"
     )
     var trueStr = strList.random()
-    var opisanie : String
+    var opisanie: String
     var current = ""
     when (trueStr) {
         "арбуз" -> {
             current = "*****"
-            opisanie = "Однолетнее травянистое растение семейства тыквенные, бахчевая культура.\n Имеет тонкие, длинные ползучие стебли, длиной до 4 м и более.\n Молодые стебли густо опушены мягкими волосками.\n Плод обычно называется ягодой."
+            opisanie =
+                "Однолетнее травянистое растение семейства тыквенные, бахчевая культура.\n Имеет тонкие, длинные ползучие стебли, длиной до 4 м и более.\n Молодые стебли густо опушены мягкими волосками.\n Плод обычно называется ягодой."
         }
         "сказка" -> {
             current = "******"
-            opisanie = "Один из жанров фольклора, либо литературы.\n Эпическое, преимущественно прозаическое произведение с волшебным, героическим или бытовым сюжетом."
+            opisanie =
+                "Один из жанров фольклора, либо литературы.\n Эпическое, преимущественно прозаическое произведение с волшебным, героическим или бытовым сюжетом."
         }
         "роль" -> {
             current = "****"
-            opisanie = "В широком смысле, это описание ограниченного множества действий,\n выполняемых кем-то или чем-то в рамках определённого процесса."
+            opisanie =
+                "В широком смысле, это описание ограниченного множества действий,\n выполняемых кем-то или чем-то в рамках определённого процесса."
         }
         "параметр" -> {
             current = "********"
@@ -47,14 +51,14 @@ fun game() {
     println("Игра начинается!")
     Thread.sleep(500)
     while (n == 0) {
+        println("Ваше количество очков: $points")
+        Thread.sleep(500)
         println("Задуманное слово: $current")
         Thread.sleep(500)
         println("Крутите барабан!")
         Thread.sleep(500)
         println("*Барабан долго крутится*")
-        var points = listOf<Int>(
-            0,
-            0,
+        var sectors = listOf<Int>(
             5,
             5,
             10,
@@ -68,13 +72,49 @@ fun game() {
             500,
             1000,
             -1,
-            -2
+            -2,
+            -3,
+            -4,
+            -5
         )
-        var gift = points.random()
+        var gift = sectors.random()
         Thread.sleep(3000)
         when (gift) {
-            -1 -> {
+            -1 -> { //приз
+                println("Сектор \"Приз\"!")
+                Thread.sleep(500)
                 println("Ура! Подарочек в студию!")
+                errCounter - 1
+                points += 250
+            }
+            -2 -> { //плюс
+                println("Сектор \"Плюс\"!")
+                Thread.sleep(500)
+                println("Поздравляем!")
+                Thread.sleep(500)
+                println("Теперь вы можете открыть ЛЮБУЮ букву в слове!")
+                Thread.sleep(500)
+                println("Назовите номер буквы, которую вы хотите открыть...")
+                Thread.sleep(500)
+                var index = readLine()!!.toInt() - 1
+                if (index == -1) {
+                    index++
+                }
+                Thread.sleep(500)
+                for (i in 0 until trueStr.length) {
+                    var b = current.split("").toMutableList()
+                    b[index] = trueStr[index].toString()
+                    current = b.toString()
+                }
+            }
+            -3 -> { //x2
+                println("Сектор \"x2\"!")
+                Thread.sleep(500)
+                println("Да нужели? Правда?")
+                Thread.sleep(500)
+                println("Все ваши очки умножаются в 2 раза!")
+                Thread.sleep(500)
+                points *= 2
             }
         }
         println("Что будете вводить: слово или букву? (1/2)")
@@ -90,6 +130,8 @@ fun game() {
                     Thread.sleep(1500)
                     println("Но не в то! ;D")
                     errCounter += 1
+                } else if (finalAnswer == trueStr) {
+
                 }
             }
             "2" -> {
